cc04d13beb96f2db7354c4dd97ac7b3349857207
CristianOvalle <cristianovallegutierrez@gmail.com>
2021-12-13 13:29:46 -0400

    b

---
+++ Crud/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        MySql.Data.MySqlClient.MySqlConnectionStringBuilder Mibase = new MySql.Data.MySqlClient.MySqlConnectionStringBuilder();
+        Mibase.Database = "pruebaconexion";
+            Mibase.Password = "";
+            Mibase.UserID = "root";
+            Mibase.Server = "localhost";
+            Mibase.IntegratedSecurity = false;
+            //Mibase.Port = 3306;
+
+            string Muestra = Mibase.ConnectionString;
+
+
+    }
+}
