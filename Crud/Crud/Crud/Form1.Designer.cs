c60b43ed3a9231bb1d67adfd4a38da801d6f67b8
CristianOvalle <cristianovallegutierrez@gmail.com>
2021-12-14 10:23:26 -0400

    practica

--- Crud/Crud/Crud/Form1.Designer.cs
+++ Crud/Crud/Crud/Form1.Designer.cs
@@ -29,21 +29,199 @@ namespace Crud
         /// </summary>
         private void InitializeComponent()
         {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.iconPictureBox1 = new FontAwesome.Sharp.IconPictureBox();
+            this.iconPictureBox2 = new FontAwesome.Sharp.IconPictureBox();
+            this.iconPictureBox3 = new FontAwesome.Sharp.IconPictureBox();
+            this.iconPictureBox4 = new FontAwesome.Sharp.IconPictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.iconPictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.iconPictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.iconPictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.iconPictureBox4)).BeginInit();
             this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3});
+            this.dataGridView1.Location = new System.Drawing.Point(26, 86);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(343, 255);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Id";
+            this.Column1.Name = "Column1";
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Nombre";
+            this.Column2.Name = "Column2";
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "cedula";
+            this.Column3.Name = "Column3";
+            // 
+            // iconPictureBox1
+            // 
+            this.iconPictureBox1.BackColor = System.Drawing.SystemColors.Control;
+            this.iconPictureBox1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.iconPictureBox1.IconChar = FontAwesome.Sharp.IconChar.UserPlus;
+            this.iconPictureBox1.IconColor = System.Drawing.SystemColors.ControlText;
+            this.iconPictureBox1.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.iconPictureBox1.IconSize = 66;
+            this.iconPictureBox1.Location = new System.Drawing.Point(674, 277);
+            this.iconPictureBox1.Name = "iconPictureBox1";
+            this.iconPictureBox1.Size = new System.Drawing.Size(73, 66);
+            this.iconPictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.iconPictureBox1.TabIndex = 1;
+            this.iconPictureBox1.TabStop = false;
+            this.iconPictureBox1.Click += new System.EventHandler(this.iconPictureBox1_Click);
+            // 
+            // iconPictureBox2
+            // 
+            this.iconPictureBox2.BackColor = System.Drawing.SystemColors.Control;
+            this.iconPictureBox2.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.iconPictureBox2.IconChar = FontAwesome.Sharp.IconChar.Trash;
+            this.iconPictureBox2.IconColor = System.Drawing.SystemColors.ControlText;
+            this.iconPictureBox2.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.iconPictureBox2.IconSize = 65;
+            this.iconPictureBox2.Location = new System.Drawing.Point(596, 277);
+            this.iconPictureBox2.Name = "iconPictureBox2";
+            this.iconPictureBox2.Size = new System.Drawing.Size(65, 65);
+            this.iconPictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.iconPictureBox2.TabIndex = 2;
+            this.iconPictureBox2.TabStop = false;
+            this.iconPictureBox2.Click += new System.EventHandler(this.iconPictureBox2_Click);
+            // 
+            // iconPictureBox3
+            // 
+            this.iconPictureBox3.BackColor = System.Drawing.SystemColors.Control;
+            this.iconPictureBox3.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.iconPictureBox3.IconChar = FontAwesome.Sharp.IconChar.Edit;
+            this.iconPictureBox3.IconColor = System.Drawing.SystemColors.ControlText;
+            this.iconPictureBox3.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.iconPictureBox3.IconSize = 64;
+            this.iconPictureBox3.Location = new System.Drawing.Point(516, 277);
+            this.iconPictureBox3.Name = "iconPictureBox3";
+            this.iconPictureBox3.Size = new System.Drawing.Size(64, 65);
+            this.iconPictureBox3.TabIndex = 3;
+            this.iconPictureBox3.TabStop = false;
+            this.iconPictureBox3.Click += new System.EventHandler(this.iconPictureBox3_Click);
+            // 
+            // iconPictureBox4
+            // 
+            this.iconPictureBox4.BackColor = System.Drawing.SystemColors.Control;
+            this.iconPictureBox4.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.iconPictureBox4.IconChar = FontAwesome.Sharp.IconChar.WindowClose;
+            this.iconPictureBox4.IconColor = System.Drawing.SystemColors.ControlText;
+            this.iconPictureBox4.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.iconPictureBox4.IconSize = 60;
+            this.iconPictureBox4.Location = new System.Drawing.Point(428, 277);
+            this.iconPictureBox4.Name = "iconPictureBox4";
+            this.iconPictureBox4.Size = new System.Drawing.Size(60, 65);
+            this.iconPictureBox4.TabIndex = 4;
+            this.iconPictureBox4.TabStop = false;
+            this.iconPictureBox4.Click += new System.EventHandler(this.iconPictureBox4_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(26, 55);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(343, 20);
+            this.textBox1.TabIndex = 5;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Stencil", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(569, 109);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 16);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Nombre";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Stencil", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(569, 172);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 16);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Cedula";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(572, 132);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(124, 20);
+            this.textBox2.TabIndex = 8;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(572, 198);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(124, 20);
+            this.textBox3.TabIndex = 9;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.iconPictureBox4);
+            this.Controls.Add(this.iconPictureBox3);
+            this.Controls.Add(this.iconPictureBox2);
+            this.Controls.Add(this.iconPictureBox1);
+            this.Controls.Add(this.dataGridView1);
             this.Name = "Form1";
             this.Text = "Form1";
-          
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.iconPictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.iconPictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.iconPictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.iconPictureBox4)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private FontAwesome.Sharp.IconPictureBox iconPictureBox1;
+        private FontAwesome.Sharp.IconPictureBox iconPictureBox2;
+        private FontAwesome.Sharp.IconPictureBox iconPictureBox3;
+        private FontAwesome.Sharp.IconPictureBox iconPictureBox4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
     }
 }
 
--- Crud/Crud/Crud/Form1.cs
+++ Crud/Crud/Crud/Form1.cs
@@ -7,18 +7,172 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
 
 namespace Crud
 {
     public partial class Form1 : Form
     {
-        private  
+        private MySqlConnection Conector = null;
+        private string id;
+
 
         public Form1()
+
         {
             InitializeComponent();
+            this.leer();
         }
 
-       
+        public void conectar()
+        {
+
+            MySqlConnectionStringBuilder mySqConnectionStringBuilder = new MySqlConnectionStringBuilder();
+            mySqConnectionStringBuilder.Database = "pruebaconexion";
+            mySqConnectionStringBuilder.Password = "";
+            mySqConnectionStringBuilder.UserID = "root";
+            mySqConnectionStringBuilder.Server = "localhost";
+            mySqConnectionStringBuilder.IntegratedSecurity = false;
+
+            Conector = new MySqlConnection(mySqConnectionStringBuilder.ConnectionString);
+            Conector.Open();
+
+
+        }
+
+        public void cerrar()
+        {
+            this.Conector.Close();
+
+        }
+
+        public void buscador()
+        {
+
+            leer($"where nombre like '%{this.textBox1.Text}%' ");
+
+        }
+
+        public void leer(string buscar = "")
+        {
+            this.conectar();
+            MySqlCommand mySqlCommand = this.Conector.CreateCommand();
+            mySqlCommand.CommandText = $"select *from personas {buscar} ";
+            MySqlDataReader mySqlDataReader = mySqlCommand.ExecuteReader();
+
+            this.dataGridView1.Rows.Clear();
+            while (mySqlDataReader.Read())
+            {
+                this.dataGridView1.Rows.Add(
+                    mySqlDataReader["id"].ToString(),
+                    mySqlDataReader["nombre"].ToString(),
+                    mySqlDataReader["cedula"].ToString()
+                    );
+            }
+            this.cerrar();
+
+
+        }
+
+        public void insertar()
+        {
+
+            this.conectar();
+            MySqlCommand mySqlCommand = this.Conector.CreateCommand();
+            mySqlCommand.CommandText = "insert into personas(nombre,cedula) values(@nombre,@cedula);";
+            mySqlCommand.Parameters.Add(new MySqlParameter("@nombre", this.textBox2.Text));
+            mySqlCommand.Parameters.Add(new MySqlParameter("@cedula", this.textBox3.Text));
+            int registroafectados = mySqlCommand.ExecuteNonQuery();
+            MessageBox.Show(registroafectados.ToString());
+
+            this.cerrar();
+
+        }
+
+        public void update()
+        {
+            this.conectar();
+            MySqlCommand mySqlCommand = this.Conector.CreateCommand();
+            mySqlCommand.CommandText = "update personas set nombre = @nombre,cedula = @cedula where id = id ; ";
+            mySqlCommand.Parameters.Add(new MySqlParameter("@nombre",
+            this.textBox1.Text));
+            mySqlCommand.Parameters.Add(new MySqlParameter("@cedula",
+            this.textBox1.Text));
+            mySqlCommand.Parameters.Add(new MySqlParameter("@id", this.id));
+            int registrosAfectados = mySqlCommand.ExecuteNonQuery();
+            MessageBox.Show(registrosAfectados.ToString());
+            this.cerrar();
+        }
+
+        public void delete()
+        {
+            DialogResult respuestaUsuario = MessageBox.Show(
+            $"Realmente desea eliminar este registro { this.textBox1.Text} con cedula { this.textBox2.Text} ", "Confirmacion",
+           MessageBoxButtons.YesNo,
+           MessageBoxIcon.Question);
+            if (respuestaUsuario == DialogResult.Yes)
+            {
+                this.conectar();
+                MySqlCommand mySqlCommand = this.Conector.CreateCommand();
+                mySqlCommand.CommandText = "delete from personas where id = @id; ";
+                mySqlCommand.Parameters.Add(new MySqlParameter("@id", this.id));
+                int registrosAfectados = mySqlCommand.ExecuteNonQuery();
+                MessageBox.Show(registrosAfectados.ToString());
+                this.cerrar();
+            }
+
+
+        }
+
+        public void cancel()
+        {
+            this.id = "0";
+            this.textBox2.Text = "";
+            this.textBox3.Text = "";
+        }
+
+        private void iconPictureBox1_Click(object sender, EventArgs e)
+        {
+            this.insertar();
+            this.leer();
+            this.cancel();
+        }
+
+        private void dataGridView1_CellDoubleClick(object sender,DataGridViewCellEventArgs e)
+        {
+            this.id = this.dataGridView1.CurrentRow.Cells[0].Value.ToString
+            ();//columna 0 igual id
+            this.textBox1.Text = this.dataGridView1.CurrentRow.Cells
+            [1].Value.ToString();//columna 1 nombre
+            this.textBox1.Text = this.dataGridView1.CurrentRow.Cells
+            [2].Value.ToString();//columna 2 cedula
+        }
+
+        private void iconPictureBox4_Click(object sender, EventArgs e)
+        {
+            this.cancel();
+
+        }
+
+        private void iconPictureBox3_Click(object sender, EventArgs e)
+        {
+            this.Update();
+            this.leer();
+            this.cancel();
+        }
+
+        private void iconPictureBox2_Click(object sender, EventArgs e)
+        {
+            this.delete();
+            this.leer();
+            this.cancel();
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            this.buscador();
+        }
     }
+
 }
