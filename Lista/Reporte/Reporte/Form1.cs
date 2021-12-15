e4d4c76075b50f4126a2d65207b1c97b64243448
CristianOvalle <cristianovallegutierrez@gmail.com>
2021-12-15 13:50:12 -0400

    reporte

---
+++ Lista/Reporte/Reporte/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Reporte
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            List<Producto> productos = new List<Producto>();
+
+            productos.Add(new Producto(1,"Leche Milex",4, 1, 567.12));
+
+            Microsoft.Reporting.WinForms.ReportDataSource reportDataSource = new Microsoft.Reporting.WinForms.ReportDataSource("dataset", productos);
+
+
+
+
+
+            this.reportViewer1.LocalReport.ReportPath = @"C:\Users\infotep\Documents\Diagrama cristian\PracticasPOO\practicaspoo2021\Lista\Reporte\Reporte\";
+            this.reportViewer1.LocalReport.DataSources.Add(reportDataSource);
+
+            this.reportViewer1.RefreshReport();
+        }
+    }
+}
---
+++ Lista/Reporte/Reporte/Producto.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reporte
+{
+    class Producto
+    {
+
+        public int id { set; get; }
+
+        public string nombre { set; get; }
+
+        public float precio { set; get; }
+
+        public float cantidad { set; get; }
+
+        public double subTotal { set; get; }
+
+
+
+        public Producto(int ID, string Nombre, float Precio, float Cantidad, double Subtotal)
+        {
+
+            id = ID;
+            nombre = Nombre;
+            precio = Precio;
+            cantidad = Cantidad;
+            subTotal = Subtotal;
+
+        }
+
+    }
+}
