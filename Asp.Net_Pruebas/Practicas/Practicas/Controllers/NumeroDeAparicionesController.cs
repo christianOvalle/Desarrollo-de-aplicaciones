ff7d80d5d73738ecbe12147d94b0cc3af8ced416
Cristian <cristianovallegutierrez@gmail.com>
2022-03-20 22:31:29 -0400

    asp

---
+++ Asp.Net_Pruebas/Practicas/Practicas/Controllers/NumeroDeAparicionesController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Practicas.Controllers
+{
+    public class NumeroDeAparicionesController : Controller
+    {
+        // GET: NumeroDeApariciones
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult RellenandoYobteniendo(int Numero)
+        {
+
+            if (Numero < 50 || Numero > 100)
+            {
+
+
+
+                return View("Index");
+
+
+            }
+            else
+            {
+
+                int[] arreglo = new int[30];
+                Random num = new Random();
+
+                for (int i = 0; i < 30; i++)
+                {
+
+                    arreglo[i] = num.Next(50, 100);
+                    
+
+
+                    ViewBag.numero += arreglo[i] + "_";
+
+                  
+                }
+                if (Numero.ToString() == ViewBag.numero)
+                {
+                    int contador = 0;
+                    contador = contador + 1;
+                    ViewBag.Repeticiones =contador;
+                }
+
+                return View();
+            }
+        }
+    }
+}
---
+++ Asp.Net_Pruebas/Practicas/Practicas/Controllers/TablaMultiController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Practicas.Controllers
+{
+    public class TablaMultiController : Controller
+    {
+        // GET: TablaMulti
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult TablaProceso(string Usuario) {
+
+
+            int numero = int.Parse(Usuario);
+
+            for (int i = numero; i<= numero; i++) {
+               
+                for (int m = 1; m <= 12; m++) {
+                    
+                    ViewBag.tabla += i + "x" + m + "=" + (m * i) +"<br>";
+                   
+
+
+                }                                    
+            }
+            
+            return View("proceso");
+
+        
+        
+        }
+
+    }
+}
