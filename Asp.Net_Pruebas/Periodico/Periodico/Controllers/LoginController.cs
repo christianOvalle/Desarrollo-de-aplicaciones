0ce2b1dfddb7e0a086a67b57258c689cf3f5a682
Cristian Ovalle <cristianovallegutierrez@gmail.com>
2022-03-22 13:44:51 -0400

    Asp

---
+++ Asp.Net_Pruebas/Periodico/Periodico/Controllers/LoginController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Periodico.Controllers
+{
+    public class LoginController : Controller
+    {
+
+        Models.db_periodicoEntities db = new Models.db_periodicoEntities();
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ProcesarLogin(string Usuario,string Clave) {
+
+           var UsuarioAux= db.Usuarios.Where(x => x.usuario1 == "Cristian" && x.clave == "1234").FirstOrDefault();
+
+            if (UsuarioAux != null)
+            {
+
+                return RedirectToAction("Index", "Usuario");
+
+            }
+            else if (UsuarioAux == null) {
+
+                return Content("No a ingresado");
+            
+            }
+
+            return View();
+        
+        
+        }
+
+
+    }
+}
