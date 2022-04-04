47952b94d3570e74d36c5510fd9d37d839cbc319
Cristian <cristianovallegutierrez@gmail.com>
2022-04-03 20:07:01 -0400

    java

--- JavaPracticas/Cristian_Java/PracticasJava10/Ejercicio9.java
+++ JavaPracticas/Cristian_Java/PracticasJava10/Ejercicio9.java
@@ -3,7 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package javaapplication1;
-import java.util.Scanner;
+
 /**
  *
  * @author Cristhian
@@ -14,25 +14,43 @@ public class Ejercicio9 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-      Scanner Obj = new Scanner(System.in);
-      int numero = 1000;
-        
-     for(int i = 2; i<numero / 2; i++){
+     
+      
+      int contador2 = 0;  
+      int contador3 = 0;
+      int contador5 = 0;
+      int contador7 = 0;
+      
+     for(int i = 1; i<=1000; i++){
                         
-         if(numero % i == 0 ){
-     
-     System.out.println(i+" No es Es Primo");
-     
-     }
+        if(i%2 ==0){
+        contador2++;
+        System.out.println(i+" Es Divisible de 2");
+        }
          
-         else {
-             
-             System.out.println(i+" Si es Es Primo");
-                 
-         }
+         if(i%3 ==0){
+        contador3++;
+        System.out.println(i+" Es Divisible de 3");
+        }
+        
+        if(i%5 ==0){
+        contador5++;
+        System.out.println(i+" Es Divisible de 5");
+        }
+        
+        if(i%7 ==0){
+        contador7++;
+        System.out.println(i+" Es Divisible de 7");
+        }
+     }
+          System.out.println("\n");
+         System.out.println(contador2+" Numero son divisibles de 2 ");
+         System.out.println(contador3+" Numero son divisibles de 3 ");
+         System.out.println(contador5+" Numero son divisibles de 5 ");
+         System.out.println(contador7+" Numero son divisibles de 7 ");
      }   
         
     }   
     
     
-     }
+     
