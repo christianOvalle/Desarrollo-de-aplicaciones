f45774f52ce05c7c35e4dff8590b7c2d5a730467
Cristian <cristianovallegutierrez@gmail.com>
2022-05-04 08:12:10 -0400

    c#

---
+++ PracticasC#/CrudRapido/CrudRapido/CrudDatos.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrudRapido
+{
+    class CrudDatos
+    {
+
+        db_personaEntities db;
+
+    }
+}
