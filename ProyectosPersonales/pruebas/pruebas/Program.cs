afff1a26042714643d6e4f8c25fb8f8471a7ae2b
Cristian Ovalle <cristianovallegutierrez@gmail.com>
2022-05-16 08:08:43 -0400

    c#

---
+++ ProyectosPersonales/pruebas/pruebas/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pruebas
+{
+    public class Program
+    {
+
+     
+        public static string calculer( string num1, string num2)
+        {
+
+           
+            
+            int r = Convert.ToInt32(num1) + Convert.ToInt32(num2);
+
+
+
+            Console.WriteLine(r);
+            Console.ReadKey();
+
+            return r.ToString();
+        }
+        public static void Main(string[] args)
+        {
+            calculer(Console.ReadLine(),Console.ReadLine());
+
+          }
+       
+
+    }
+}
